058ca7df9fae6fed75bd6194de83f77736763e91
shimomo <yuichi@shimomo.net>
2025-08-08 22:03:13 +0900

    test: Add Jest testing framework

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+};
+
+export default config;
